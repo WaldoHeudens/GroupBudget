62e2d5ed145cda4f95d8f5126cebfdb319842ecb
Waldo Heudens <waldo@heudens.be>
2024-10-15 11:06:05 +0200

    Add and Save changes in datagrid without controlling the controls

--- GroupBudget_WPF/MainWindow.xaml.cs
+++ GroupBudget_WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using GroupBudget_WPF.Models;
+﻿using GroupBudget_WPF.Migrations;
+using GroupBudget_WPF.Models;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -28,6 +29,42 @@ namespace GroupBudget_WPF
 
         private void tiPeople_GotFocus(object sender, RoutedEventArgs e)
         {
+            InitDgPersons();
+        }
+
+
+        private void dgPersons_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            PersonDatagridViewModel person = (PersonDatagridViewModel)dgPersons.CurrentItem;
+            if (person != null)
+            {
+                dgPersons.SelectedItem = person; 
+                tbFirstName.Text = person.FirstName;
+                tbLastName.Text = person.LastName;
+                tbUserName.Text = person.Name;
+            }
+        }
+
+        private void btAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(string.IsNullOrEmpty(tbFirstName.Text)
+                || string.IsNullOrEmpty(tbLastName.Text)
+                || string.IsNullOrEmpty(tbUserName.Text)))
+            {
+                Person p = App.Context.Persons.FirstOrDefault(p => p.Name == tbUserName.Text);
+                if (p == null)
+                {
+                    Person person = new Person { FirstName = tbFirstName.Text, LastName = tbLastName.Text, Name = tbUserName.Text };
+                    //App.Context.Persons.Add(person);   // You can assign specifically
+                    App.Context.Add(person);
+                    App.Context.SaveChanges();
+                    InitDgPersons();
+                }
+            }
+        }
+
+        private void InitDgPersons()
+        {
             dgPersons.ItemsSource = (from person in App.Context.Persons
                                      where person.Deleted > DateTime.Now
                                      orderby person.LastName, person.FirstName
@@ -42,13 +79,18 @@ namespace GroupBudget_WPF
             dgPersons.Columns[3].Header = "Last name";
         }
 
-
-        private void dgPersons_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        private void btSave_Click(object sender, RoutedEventArgs e)
         {
-            PersonDatagridViewModel person = (PersonDatagridViewModel)dgPersons.CurrentItem;
+            Person person = App.Context.Persons.FirstOrDefault(p => p.Name == tbUserName.Text);
+            //Person person = App.Context.Persons.FirstOrDefault(p => p.Id == ((Person)dgPersons.SelectedItem).Id);
             if (person != null)
             {
-                int id = person.Id;
+                person.FirstName = tbFirstName.Text;
+                person.LastName = tbLastName.Text;
+                App.Context.Update(person);
+                App.Context.SaveChanges();
+                InitDgPersons();
+
             }
         }
     }
