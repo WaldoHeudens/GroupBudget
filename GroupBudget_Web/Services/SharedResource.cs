4f1703d6882315f1cd488c03b22df5cd51c3428b
Waldo Heudens <waldo@heudens.be>
2024-12-04 12:26:05 +0100

    Gebruik van een shared resource voor wederkerende teksten Belangrijk is de SharedResource.cs -class in the basis namespace!

---
+++ GroupBudget_Web/Services/SharedResource.cs
@@ -0,0 +1,7 @@
+﻿namespace GroupBudget_Web
+{
+    // Dit is een lege class die aan de basis-namespace wordt toegevoegd om de shard resource localizer file te beheren
+    public class SharedResource
+    {
+    }
+}
